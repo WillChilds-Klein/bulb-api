b5c96c835039e64ae63746c65f1ee132dd6cd351
WillChilds-Klein <willck93@gmail.com>
2017-04-14 19:51:57 -0400

    Add basic in-memory API implementation.
    DO NOT USE THIS FOR PRODUCTION. This is a cursory implementation of our
    API. Its only persistence is a few python dicts, which means that it
    will not return data consistently when deployed via lambda.

--- app.py
+++ app.py
@@ -2,51 +2,119 @@ import connexion
 
 from connexion import NoContent
 
-def get_organization(pet_id):
-    return (NoContent, 405)
 
-def update_organization(pet_id):
-    return (NoContent, 405)
+org_db = {}
+doc_db = {}
+user_db = {}
 
-def delete_organization(pet_id):
-    return (NoContent, 405)
 
-def list_organizations(pet_id):
-    return (NoContent, 405)
+def get_organization(id):
+    org_id = id
+    print org_id, type(org_id)
+    print [type(key) for key in org_db.keys()]
+    if org_id in org_db:
+        return org_db[id], 200
+    return NoContent, 404
 
-def create_organization(pet_id):
-    return (NoContent, 405)
 
-def get_document(pet_id):
-    return (NoContent, 405)
+def update_organization(body, id):
+    org_id = body['id']
+    if org_id in org_db:
+        org_db[org_id] = body
+        return NoContent, 200
+    return NoContent, 404
 
-def update_document(pet_id):
-    return (NoContent, 405)
 
-def delete_document(pet_id):
-    return (NoContent, 405)
+def delete_organization(id):
+    org_id = id
+    if org_id in org_db:
+        del org_db[org_id]
+        return NoContent, 200
+    return NoContent, 404
 
-def list_documents(pet_id):
-    return (NoContent, 405)
 
-def create_document(pet_id):
-    return (NoContent, 405)
+def list_organizations():
+    return org_db.values()
 
-def get_user(pet_id):
-    return (NoContent, 405)
 
-def update_user(pet_id):
-    return (NoContent, 405)
+def create_organization(body):
+    org_id = body['id']
+    org_db[org_id] = body
+    return 'Success!', 200
 
-def delete_user(pet_id):
-    return (NoContent, 405)
 
-def list_users(pet_id):
-    return (NoContent, 405)
+def get_document(id):
+    doc_id = id
+    if doc_id in doc_db:
+        return doc_db[id], 200
+    return NoContent, 404
 
-def create_user(pet_id):
-    return (NoContent, 405)
 
-app = connexion.App(__name__)
-app.add_api('spec/swagger.yaml')
-app.run(port=5000)
+def update_document(body, id):
+    doc_id = body['id']
+    if doc_id in doc_db:
+        doc_db[doc_id] = body
+        return NoContent, 200
+    return NoContent, 404
+
+
+def delete_document(id):
+    doc_id = id
+    if doc_id in doc_db:
+        del doc_db[doc_id]
+        return NoContent, 200
+    return NoContent, 404
+
+
+def list_documents():
+    return doc_db.values()
+
+
+def create_document(body):
+    doc_id = body['id']
+    doc_db[doc_id] = body
+    return 'Success!', 200
+
+
+def get_user(id):
+    user_id = id
+    print user_id, type(user_id)
+    print [type(key) for key in user_db.keys()]
+    if user_id in user_db:
+        return user_db[id], 200
+    return NoContent, 404
+
+
+def update_user(body, id):
+    user_id = body['id']
+    if user_id in user_db:
+        user_db[user_id] = body
+        return NoContent, 200
+    return NoContent, 404
+
+
+def delete_user(id):
+    user_id = id
+    if user_id in user_db:
+        del user_db[user_id]
+        return NoContent, 200
+    return NoContent, 404
+
+
+def list_users():
+    return user_db.values()
+
+
+def create_user(body):
+    user_id = body['id']
+    user_db[user_id] = body
+    return 'Success!', 200
+
+
+connexion_app = connexion.App(__name__)
+connexion_app.add_api('spec/swagger.yaml', strict_validation=True)
+
+app = connexion_app.app
+
+if __name__ == '__main__':
+    connexion_app.run(port=8080, host='0.0.0.0')
