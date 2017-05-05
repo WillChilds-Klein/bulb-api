6c0a3b78238d62bad61cec8038b6a0f136dab625
WillChilds-Klein <willck93@gmail.com>
2017-05-05 16:19:35 -0400

    Add pytest stubs for all the things.
    Pretty simple, we just add some stubs in the newly-created `tests/`
    directory. Now just need to fill them out...
    
    TESTING TODO
    ============
    - implement test fixtures to reuse setup for local DDB, entities, etc.
    - implement the damn stubs!

---
+++ tests/test_crud.py
@@ -0,0 +1,95 @@
+from api.handlers import (
+    create_entity,
+    get_entity,
+    list_entity,
+    update_entity,
+    delete_entity
+)
+
+
+# +---------+
+# | GENERAL |
+# +---------+
+
+def test_specify_bad_model():
+    pass
+
+
+# +--------+
+# | CREATE |
+# +--------+
+
+def test_create_entity_ok():
+    pass
+
+
+def test_create_entity_missing_attrs():
+    pass
+
+
+def test_create_entity_extra_attrs():
+    pass
+
+
+def test_create_entity_bad_attr_types():
+    pass
+
+
+def test_create_entity_already_exists():
+    pass
+
+
+def test_read_newly_created():
+    pass
+
+
+def test_required_attr_not_specified():
+    pass
+
+
+# +------+
+# | READ |
+# +------+
+
+def test_get_entity_ok():
+    pass
+
+
+def test_get_entity_not_found():
+    pass
+
+
+def test_list_entity_ok():
+    pass
+
+
+# +--------+
+# | UPDATE |
+# +--------+
+
+def test_update_entity_ok():
+    pass
+
+
+def test_update_entity_missing_attrs():
+    pass
+
+
+def test_update_entity_extra_attrs():
+    pass
+
+
+def test_update_entity_bad_attr_types():
+    pass
+
+
+# +--------+
+# | DELETE |
+# +--------+
+
+def test_delete_entity_ok():
+    pass
+
+
+def test_delete_entity_not_found():
+    pass
---
+++ tests/test_models.py
@@ -0,0 +1,21 @@
+from api.models import BulbModel
+
+
+def test_get_hash_key_name():
+    pass
+
+
+def test_get_unused_uuid():
+    pass
+
+
+def test_to_dict():
+    pass
+
+
+def test_update_from_dict():
+    pass
+
+
+# TODO: create little tool to test for isomorphisms between pynamo models
+#       attr types and swagger obj attr types.
